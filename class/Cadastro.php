e9f2943378b5f452896bf5f2892fd279184a36aa
Jérémy DA TRINDADE <jeremytrindade@gmail.com>
2018-04-26 12:35:50 +0200

    so funciona se meter string em //

--- class/Cadastro.php
+++ class/Cadastro.php
@@ -4,15 +4,15 @@ class Cadastro{
     private $nome;
     private $email;
     private $senha;
-    public function getNome():string
+    public function getNome()//:string
     {
         return $this->nome;
     }
-    public function getEmail():string
+    public function getEmail()//:string
     {
         return $this->email;
     }
-    public function getSenha():string
+    public function getSenha()//:string
     {
         return $this->senha;
     }
@@ -28,7 +28,7 @@ class Cadastro{
     public function __toString(){
         return json_encode(array(
             "nome" => $this->getNome(),
-            "email" => $this->getEamil(),
+            "email" => $this->getEmail(),
             "senha " => $this->getSenha()
         ));
     }
--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 require_once("config.php");
 
+$cad = new Cadastro();
+
 $cad->setNome("Djalma Sindeaux");
 $cad->setEmail("djalma@hcode.com.br");
 $cad->setSenha("123456");
